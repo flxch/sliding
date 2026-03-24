dba2f5a971c9beeba5b07f66276b00c4d1f94ba4
Felix Klaedtke <felix.klaedtke@gmail.com>
2026-03-24 12:08:15 +0100

    todos

--- go/doc.go
+++ go/doc.go
@@ -1,6 +1,10 @@
 package sliding
 
-// Instead of the Window type with a left and right bound, one could have two
-// functions TooClose(d T) bool and TooFar(d T bool) for a window.  This is
-// strictly more general.  First, it is easy to simulate the window bounds.
-// Second, one could implement windows based timestamps and not on indices.
+// TODOs:
+// * Instead of the Window type with a left and right bound, one could have two
+//   functions TooClose(d T) bool and TooFar(d T) bool for a window.  This is
+//   strictly more general.  First, it is easy to simulate the window bounds.
+//   Second, one could implement windows based timestamps and not on indices.
+// * Implement sliding window algorithm (associative operator).
+// * Improve API.
+// * Document code and package.
