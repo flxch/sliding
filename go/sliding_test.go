a120d298d81d5bf0c0d1073cec67b6c882fb7a26
Felix Klaedtke <felix.klaedtke@gmail.com>
2026-04-01 13:36:27 +0200

    comments

--- go/sliding_test.go
+++ go/sliding_test.go
@@ -5,6 +5,14 @@ import (
 )
 
 
+// General declarations (types and function) for the tests of the various
+// sliding algorithms.
+
+
+// A test case consists of the operator (possibly also the inverse operator),
+// the stream of elements, the positions of the sliding window, and the expected
+// aggregations for the windows.  Here, we fix for simplicity, the type of the
+// elements to int.
 type testcase struct {
     op       sliding.Op[int]
     inv      sliding.Op[int]
